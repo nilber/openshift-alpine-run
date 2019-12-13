ef974ce685b0b6671560533f9c4cdc1c504abc3e
Nilber <nilber@vittorazzi.com>
2019-12-12 23:26:56 -0300

    bkp

---
+++ build-http-server.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+DOCKER_IMAGEM=alpine-run
+S2I_IMAGEM=nbmaster/openshift-alpine-run
+TAG=http-server
+
+docker system prune
+docker rmi $DOCKER_IMAGEM:$TAG $S2I_IMAGEM:$TAG
+
+docker build src-http-server/ -t $DOCKER_IMAGEM:$TAG
+
+s2i build src-http-server/ $DOCKER_IMAGEM:$TAG $S2I_IMAGEM:$TAG
+
+docker push $S2I_IMAGEM:$TAG
