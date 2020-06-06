62252a69ccf15d6b4cd619f76e2f05785c8217f3
Nilber <nilber@vittorazzi.com>
2020-06-06 10:03:46 -0300

    bkp

--- build-ubuntu-run-with-libvips.sh
+++ build-ubuntu-run-with-libvips.sh
@@ -7,7 +7,7 @@ TAG=ubuntu-with-libvips
 # docker system prune
 # docker rmi $DOCKER_IMAGEM:$TAG $S2I_IMAGEM:$TAG
 
-# docker build src-ubuntu-run-with-libvips/ -t $DOCKER_IMAGEM:$TAG
+docker build src-ubuntu-run-with-libvips/ -t $DOCKER_IMAGEM:$TAG
 
 s2i build src-ubuntu-run-with-libvips/ $DOCKER_IMAGEM:$TAG $S2I_IMAGEM:$TAG
 
