2f97329f6c4f69afbe573b344840e9e8c30b6efe
nilber <nilber@vittorazzi.com>
2021-08-25 20:38:02 -0300

    bkp

--- build.sh
+++ build.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 
-DOCKER_IMAGEM=alpine-run
-S2I_IMAGEM=nbmaster/openshift-alpine-run
-TAG=1.0
+# DOCKER_IMAGEM=alpine-run
+# S2I_IMAGEM=nbmaster/openshift-alpine-run
+# TAG=1.0
 
-docker system prune
-docker rmi $DOCKER_IMAGEM:$TAG $S2I_IMAGEM:$TAG
+# docker system prune
+# docker rmi $DOCKER_IMAGEM:$TAG $S2I_IMAGEM:$TAG
+
+# docker build src/ -t $DOCKER_IMAGEM:$TAG
+
+# s2i build src/ $DOCKER_IMAGEM:$TAG $S2I_IMAGEM:$TAG
+
+# docker push $S2I_IMAGEM:$TAG
+
+
+
+DOCKER_IMAGEM=nbmaster/runtime-env
+TAG=go-alpine
 
 docker build src/ -t $DOCKER_IMAGEM:$TAG
-
-s2i build src/ $DOCKER_IMAGEM:$TAG $S2I_IMAGEM:$TAG
-
-docker push $S2I_IMAGEM:$TAG
+docker push $DOCKER_IMAGEM:$TAG
